7a6d4723df41432bb0aa1262b48a0aa87ffba9dd
franciscomarin98 <francmarinc@gmail.com>
2021-06-17 15:11:43 -0500

    Entendiendo la comunicacion entre componentes

---
+++ src/components/AccionesCuenta.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>{{ texto }}</button>
+</template>
+
+<script>
+export default {
+  name: "Acciones Cuenta",
+  props: {
+    texto: String,
+  },
+};
+</script>
+
+<style>
+</style>
--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -5,14 +5,23 @@
   <div v-for="servicio in servicios" :key="servicio.id">
     {{ servicio }}
   </div>
+  <h4>Acciones cuenta</h4>
+  <AccionesCuenta texto="Aumentar" />
+  &nbsp;
+  <AccionesCuenta texto="Disminuir" />
   <!-- <div v-for="(servicio, index) in servicios" :key="index">
     {{ servicio }}
   </div> -->
 </template>
 
 <script>
+import AccionesCuenta from "./AccionesCuenta.vue";
+
 export default {
   name: "Cuenta",
+  components: {
+    AccionesCuenta,
+  },
   data() {
     return {
       saldo: 2000,
@@ -20,6 +29,14 @@ export default {
       estado: true,
       servicios: ["Retiro", "Deposito", "Préstamo"],
     };
+  },
+  methods: {
+    aumentar() {
+      this.saldo = this.saldo + 100;
+    },
+    disminuir() {
+      this.saldo = this.saldo - 100;
+    },
   },
 };
 </script>
